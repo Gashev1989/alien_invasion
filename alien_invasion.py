cee8b11dac4161af185c0157c9ac5cd2e337840b
Константин Гашев <gashev1989@yandex.ru>
2024-01-30 23:51:08 +0500

    Added settings and ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,15 +2,22 @@ import sys
 
 import pygame
 
+from settings import Settings
+from ship import Ship
+
 
 class AlienInvasion:
     """Класс для управления ресурсами игры."""
     def __init__(self):
         """Инициализирует игру и создает игровые ресурсы."""
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_heigh)
+        )
         pygame.display.set_caption('Alien Invasion')
+        self.ship = Ship(self.screen)
 
     def run_game(self):
         """Запуск игры."""
@@ -18,6 +25,8 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
             pygame.display.flip()
 
 
---
+++ settings.py
@@ -0,0 +1,7 @@
+class Settings():
+    """Класс хранения настроек игры."""
+    def __init__(self):
+        """Инициализация настроек игры."""
+        self.screen_width = 1200
+        self.screen_heigh = 800
+        self.bg_color = (230, 230, 230)
