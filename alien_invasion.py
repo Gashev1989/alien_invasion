c1de88709f6c58cbd39024c14415a54ea801858c
Константин Гашев <gashev1989@yandex.ru>
2024-02-01 23:11:51 +0500

    Added methods: check_events and update_screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -22,12 +22,20 @@ class AlienInvasion:
     def run_game(self):
         """Запуск игры."""
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
+            self._check_events()
+            self._update_screen()
+
+    def _check_events(self):
+        """Обработка нажатия клавиш и событий мыши."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
                     sys.exit()
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-            pygame.display.flip()
+    
+    def _update_screen(self):
+        """Обновление изображения на экране и отображение нового экрана."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
