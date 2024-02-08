eaf80dd8c21dd910fda0a499bcb64a2a5e159254
Константин Гашев <gashev1989@yandex.ru>
2024-02-08 23:39:57 +0500

    Added limit for bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -62,8 +62,9 @@ class AlienInvasion:
 
     def _fire_bullet(self):
         """Создание нового снаряда и включение его в группу bullets."""
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
 
     def _update_screen(self):
         """Обновление изображения на экране и отображение нового экрана."""
