0e02abebf4a1b4e611d92af8c82fdf3ae6a610d8
Константин Гашев <gashev1989@yandex.ru>
2024-02-18 23:41:54 +0500

    Added alien-moving right

--- alien.py
+++ alien.py
@@ -14,7 +14,15 @@ class Alien(Sprite):
         self.rect.y = self.rect.height
         self.x = float(self.rect.x)
 
+    def check_edges(self):
+        """Проверка достижения края экрана."""
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right or self.rect.left <= 0:
+            return True
+
     def update(self):
         "Перемещает пришельца вправо."
-        self.x += self.settings.alien_speed
+        self.x += (
+            self.settings.alien_speed * self.settings.fleet_direction
+        )
         self.rect.x = self.x
