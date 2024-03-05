9ff2bf33c50b436bc0f799db6d5c0b0ce71c11f5
Константин Гашев <gashev1989@yandex.ru>
2024-03-05 21:05:15 +0500

    Added scoreboard

--- game_stats.py
+++ game_stats.py
@@ -9,3 +9,4 @@ class GameStats():
     def reset_stats(self):
         """Инициализация статистики, меняющейся в процессе игры."""
         self.ships_left = self.settings.ship_limit
+        self.score = 0
